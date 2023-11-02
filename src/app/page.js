b2627df2dd0277c70cc8d45101ff042a9688d1d9
Mike <mike.wislek@gmail.com>
2023-11-02 16:17:04 -0500

    refactor: move site metadata to layout; use title template

--- src/app/page.js
+++ src/app/page.js
@@ -2,14 +2,8 @@ import React from "react";
 
 import BlogSummaryCard from "@/components/BlogSummaryCard";
 import { getBlogPostList } from "@/helpers/file-helpers";
-import { BLOG_TITLE } from "@/constants";
 
 import styles from "./homepage.module.css";
-
-export const metadata = {
-  title: BLOG_TITLE,
-  description: "A wonderful blog about JavaScript",
-};
 
 async function Home() {
   const blogPosts = (await getBlogPostList()) ?? [];
