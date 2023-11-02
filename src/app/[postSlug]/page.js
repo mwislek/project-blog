96c6f9b71a08741c3d1c0a35cfc1459eeeaee531
Mike <mike.wislek@gmail.com>
2023-11-02 16:52:15 -0500

    feat: render CodeSnippet for pre (code) tags

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,34 +1,39 @@
-import React from 'react';
-import { MDXRemote } from 'next-mdx-remote/rsc';
+import React from "react";
+import { MDXRemote } from "next-mdx-remote/rsc";
 
-import BlogHero from '@/components/BlogHero';
-import { loadBlogPost } from '@/helpers/file-helpers';
+import BlogHero from "@/components/BlogHero";
+import CodeSnippet from "@/components/CodeSnippet";
+import { loadBlogPost } from "@/helpers/file-helpers";
 
-import styles from './postSlug.module.css';
+import styles from "./postSlug.module.css";
 
-export async function generateMetadata({ params: { postSlug }})  {
-  const blogPost = await loadBlogPost(postSlug)
-  const { frontmatter: { title, abstract: description } } = blogPost
+export async function generateMetadata({ params: { postSlug } }) {
+  const blogPost = await loadBlogPost(postSlug);
+  const {
+    frontmatter: { title, abstract: description },
+  } = blogPost;
 
   return {
     title,
     description,
-  }
+  };
 }
 
-async function BlogPost({ params: { postSlug }}) {
-  const blogPost = await loadBlogPost(postSlug)
-  const { frontmatter, content } = blogPost
-  const { title, publishedOn } = frontmatter
+async function BlogPost({ params: { postSlug } }) {
+  const blogPost = await loadBlogPost(postSlug);
+  const { frontmatter, content } = blogPost;
+  const { title, publishedOn } = frontmatter;
 
   return (
     <article className={styles.wrapper}>
-      <BlogHero
-        title={title}
-        publishedOn={publishedOn}
-      />
+      <BlogHero title={title} publishedOn={publishedOn} />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote
+          source={content}
+          components={{
+            pre: CodeSnippet
+          }}
+        />
       </div>
     </article>
   );
